3e0a57a8f53cfdd0b90657e8c3fe8b6ea72efd6a
socha23 <socha23@gmail.com>
2018-07-31 00:40:23 +0200

    added node and react

---
+++ src/main/java/pl/socha23/memba/sample/Controller.java
@@ -0,0 +1,17 @@
+package pl.socha23.memba.sample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ */
+@RestController
+public class Controller {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+}
