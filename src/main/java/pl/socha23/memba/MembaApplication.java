113642f88d5488f55d784b4fdcfdc096a5e2ac47
socha23 <socha23@gmail.com>
2018-07-30 23:28:19 +0200

    basic oauth security

--- src/main/java/pl/socha23/memba/MembaApplication.java
+++ src/main/java/pl/socha23/memba/MembaApplication.java
@@ -2,6 +2,12 @@ package pl.socha23.memba;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
 @SpringBootApplication
 public class MembaApplication {
@@ -12,4 +18,6 @@ public class MembaApplication {
         
         SpringApplication.run(MembaApplication.class, args);
     }
+
+
 }
