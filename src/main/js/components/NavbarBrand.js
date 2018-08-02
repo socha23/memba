e826f2082da8c7662f6bf717440225ced6984297
socha23 <socha23@gmail.com>
2018-08-02 21:12:01 +0200

    smaller smaller memba

--- src/main/js/components/NavbarBrand.js
+++ src/main/js/components/NavbarBrand.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
-export default () => <span className="navbar-brand">
-                <img src="memba33x30.png" width={33} height={30} className="d-inline-block align-top"/>
+export default () => [
+    <img src="memba48x44.png" width={48} height={44} style={{marginLeft: 5}} className="d-inline-block align-top"/>,
+    <span className="navbar-brand">
                 <span style={{marginLeft: 10}}>
                 Memba
                 </span>
-            </span>
+            </span>]
 
 
