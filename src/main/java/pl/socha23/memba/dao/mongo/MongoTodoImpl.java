3ee35591ed2482ab053b796a687d15c8723d8363
Przemysław Socha <przemyslaw.socha@asseco.pl>
2018-08-08 14:09:55 +0200

    @Document fix

--- src/main/java/pl/socha23/memba/dao/mongo/MongoTodoImpl.java
+++ src/main/java/pl/socha23/memba/dao/mongo/MongoTodoImpl.java
@@ -4,7 +4,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import pl.socha23.memba.business.api.model.BasicTodo;
 import pl.socha23.memba.business.api.model.Todo;
 
-@Document("todo")
+@Document(collection = "todo")
 class MongoTodoImpl extends BasicTodo implements Todo {
 
     public static MongoTodoImpl copy(Todo todo) {
