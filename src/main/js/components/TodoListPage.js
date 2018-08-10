2f4d25299389421818f0e5b228882a484c130299
Przemysław Socha <przemyslaw.socha@asseco.pl>
2018-08-10 12:37:52 +0200

    added groupId in todo list

--- src/main/js/components/TodoListPage.js
+++ src/main/js/components/TodoListPage.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+
+import {withRouterWithQuery, LinkWithQuery} from "../routerUtils";
 
 import todoLogic from '../todoLogic'
 import TodoList from './TodoList'
@@ -7,31 +8,6 @@ import {BrandedNavbar} from './PageTopNavbar'
 import {BorderlessBottomNavbar} from "./PageBottomNavbar";
 import PageBody from './PageBody'
 import ButtonIcon from './ButtonIcon'
-
-
-const TodoListView = ({
-                          todos = [],
-                          showCompleted = false,
-                          onToggleShowCompleted = () => {},
-                      }) =>
-    <div>
-        <BrandedNavbar>
-            <ul className="navbar-nav">
-                <li className={"nav-item" + (showCompleted ? " active" : "")} onClick={() => onToggleShowCompleted()}>
-                    <i style={{fontSize: 20}} className="nav-link far fa-check-square"/>
-                </li>
-            </ul>
-        </BrandedNavbar>
-        <PageBody>
-            <TodoList todos={todos}/>
-        </PageBody>
-        <BorderlessBottomNavbar>
-            <Link to="/add" className="btn btn-block btn-lg btn-info">
-                <ButtonIcon className="fas fa-plus"/>Add new...
-            </Link>
-        </BorderlessBottomNavbar>
-    </div>
-;
 
 class TodoListPage extends React.Component {
     state = {
@@ -41,10 +17,15 @@ class TodoListPage extends React.Component {
 
     render() {
         return <TodoListView
-            todos={todoLogic.listTodos({showCompleted: this.state.showCompleted})}
+            groupId={this.getGroupId()}
+            todos={todoLogic.listTodos({groupId: this.getGroupId(), showCompleted: this.state.showCompleted})}
             showCompleted={this.state.showCompleted}
             onToggleShowCompleted={() => this.onToggleShowCompleted()}
         />
+    }
+
+    getGroupId() {
+        return this.props.location.query.groupId || todoLogic.ROOT_GROUP_ID
     }
 
     onToggleShowCompleted() {
@@ -62,8 +43,32 @@ class TodoListPage extends React.Component {
     incGeneration() {
         this.setState({generation: this.state.generation + 1})
     }
-
-
 }
 
-export default TodoListPage
+export default withRouterWithQuery(TodoListPage)
+
+
+const TodoListView = ({
+                          groupId = todoLogic.ROOT_GROUP_ID,
+                          todos = [],
+                          showCompleted = false,
+                          onToggleShowCompleted = () => {},
+                      }) =>
+    <div>
+        <BrandedNavbar>
+            <ul className="navbar-nav">
+                <li className={"nav-item" + (showCompleted ? " active" : "")} onClick={() => onToggleShowCompleted()}>
+                    <i style={{fontSize: 20}} className="nav-link far fa-check-square"/>
+                </li>
+            </ul>
+        </BrandedNavbar>
+        <PageBody>
+            <TodoList todos={todos}/>
+        </PageBody>
+        <BorderlessBottomNavbar>
+            <LinkWithQuery to="/add" query={{groupId: groupId}} className="btn btn-block btn-lg btn-info">
+                <ButtonIcon className="fas fa-plus"/>Add new...
+            </LinkWithQuery>
+        </BorderlessBottomNavbar>
+    </div>
+;
---
+++ src/main/js/routerUtils.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import {compose, withPropsOnChange} from 'recompose'
+import {Link, withRouter} from 'react-router-dom'
+import queryString from 'query-string'
+
+const propsWithQuery = withPropsOnChange(
+    ['location', 'match'],
+    ({location, match}) => {
+        return {
+            location: {
+                ...location,
+                query: queryString.parse(location.search)
+            },
+            match
+        };
+    }
+);
+
+export const withRouterWithQuery = compose(withRouter, propsWithQuery);
+
+export const LinkWithQuery = (params) => {
+    const to = params.to;
+    const search = params.query ? ("?" + queryString.stringify(params.query)) : "";
+    
+    var newTo = {};
+    if (typeof to === 'object') {
+        newTo = {
+            ...to,
+            search: search
+        }
+    } else {
+        newTo = {
+            pathname: params.to,
+            search: search
+        }
+    }
+    return <Link  {...params} to={newTo}>{params.children}</Link>
+};
+
