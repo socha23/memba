c1f55e2572daf68e326d86d72786250c0b5df43a
socha23 <socha23@gmail.com>
2018-08-12 20:15:22 +0200

    clientside todo list refactor part one

--- src/main/js/components/TodoListPage.js
+++ src/main/js/components/TodoListPage.js
@@ -18,14 +18,13 @@ class TodoListPage extends React.Component {
 
     render() {
         return <div>
-            <TodoListView
+            <TodoListNavbar
                 groupId={this.getGroupId()}
-                groups={todoLogic.listGroups({groupId: this.getGroupId()})}
-                todos={todoLogic.listTodos({groupId: this.getGroupId(), showCompleted: this.state.showCompleted})}
                 showCompleted={this.state.showCompleted}
                 onToggleShowCompleted={() => this.onToggleShowCompleted()}
-                onClickEditGroup={() => this.onEditGroup()}
             />
+            <TodoListView showCompleted={this.state.showCompleted} groupId={this.getGroupId()}/>
+            <TodoListBottomToolbar groupId={this.getGroupId()} addEnabled={true}/>
         </div>
     }
 
@@ -38,24 +37,14 @@ class TodoListPage extends React.Component {
     }
 
     componentDidMount() {
-        todoLogic.subscribe(this, () => this.incGeneration())
+        todoLogic.subscribe(this, () => {this.setState({generation: this.state.generation + 1})})
     }
 
     componentWillUnmount() {
         todoLogic.unsubscribe(this)
     }
-
-    incGeneration() {
-        this.setState({generation: this.state.generation + 1})
-    }
-
-    onEditGroup() {
-        this.hideModal();
-        this.props.history.push(encodeQuery("/group/" + this.getGroupId(), {groupId: this.getGroupId()}))
-    }
 }
-
-export default withRouterWithQuery(TodoListPage)
+export default withRouterWithQuery(TodoListPage);
 
 function isRoot(groupId) {
     return groupId === todoLogic.ROOT_GROUP_ID;
@@ -63,15 +52,26 @@ function isRoot(groupId) {
 
 const TodoListView = ({
                           groupId = todoLogic.ROOT_GROUP_ID,
-                          todos = [],
-                          groups = [],
                           showCompleted = false,
-                          onToggleShowCompleted = () => {},
-                          onClickAdd = () => {},
-                          onLongClickAdd = () => {},
-                          onClickEditGroup = () => {},
                       }) => {
-    var navbarFirstElem;
+
+    const groups = todoLogic.listGroups({groupId: groupId});
+    const todos = todoLogic.listTodos({groupId: groupId, showCompleted: showCompleted});
+
+    return <PageBody>
+            <GroupList groups={groups}/>
+            <TodoList todos={todos}/>
+        </PageBody>
+};
+
+const TodoListNavbar = withRouterWithQuery(({
+                                                groupId = todoLogic.ROOT_GROUP_ID,
+                                                showCompleted = false,
+                                                onToggleShowCompleted,
+                                                history
+                                            }) => {
+
+    let navbarFirstElem;
     if (isRoot(groupId)) {
         navbarFirstElem = <MembaIconAndTitle/>;
     } else {
@@ -88,25 +88,23 @@ const TodoListView = ({
         onClick={() => onToggleShowCompleted()}
     />;
 
-    const editGroupButton = isRoot(groupId) ? <span/> : <ToolbarButton className="fas fa-cog" onClick={() => onClickEditGroup()}/>;
+    const editGroupButton = isRoot(groupId) ? <span/> :
+        <ToolbarButton
+            className="fas fa-cog"
+            onClick={() => {history.push(encodeQuery("/group/" + groupId, {groupId: groupId}))}}
+        />;
 
-    return <div>
-        <PageTopNavbar>
-            {navbarFirstElem}
-            <div className="btn-toolbar">
-                {editGroupButton}
-                {showCompletedButton}
-            </div>
-        </PageTopNavbar>
-        <PageBody>
-            <GroupList groups={groups}/>
-            <TodoList todos={todos}/>
-        </PageBody>
-        <BorderlessBottomNavbar>
-            <AddItemButton groupId={groupId}/>
-        </BorderlessBottomNavbar>
-    </div>
-};
+    return <PageTopNavbar>
+        {navbarFirstElem}
+        <div className="btn-toolbar">
+            {editGroupButton}
+            {showCompletedButton}
+        </div>
+    </PageTopNavbar>
+});
 
+const TodoListBottomToolbar= ({addEnabled, groupId}) => addEnabled ? <BorderlessBottomNavbar>
+    <AddItemButton groupId={groupId}/>
+</BorderlessBottomNavbar> : <span/>;
 
 
