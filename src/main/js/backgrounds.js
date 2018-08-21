0f926601cd4836acf76dbd8d14bdf21da312bb5c
socha23 <socha23@gmail.com>
2018-08-22 00:58:39 +0200

    group backgrounds

--- src/main/js/backgrounds.js
+++ src/main/js/backgrounds.js
@@ -1,6 +1,19 @@
 const backgrounds = [
     {name: "(none)", value: "none"},
-    {name: "Groceries 1", value: "groceries1.jpg"}
+    {name:"Groceries 1", value:"groceries1.jpg"},
+    {name:"Groceries 2", value:"groceries2.jpg"},
+    {name:"Chemistry 1", value:"chemistry1.jpg"},
+    {name:"Chemistry 1", value:"chemistry2.jpg"},
+    {name:"Stationery", value:"stationery.jpg"},
+    {name:"Appointment 1", value:"appointment1.jpg"},
+    {name:"Appointment 2", value:"appointment2.jpg"},
+    {name:"Car", value:"car.jpg"},
+    {name:"Plane", value:"plane.jpg"},
+    {name:"Pills", value:"pills.jpg"},
+    {name:"Gifts", value:"gifts.jpg"},
+    {name:"Bills", value:"bills.jpg"},
+    {name:"Parcels", value:"parcels.jpg"},
+    {name:"Cogs", value:"cogs.jpg"},
 ];
 
 export const backgroundNoneValue = backgrounds[0].value;
