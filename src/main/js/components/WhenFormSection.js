1a49a6d743fdcdf5e0679c61a649fd76012fe65b
socha23 <socha23@gmail.com>
2018-08-30 22:28:38 +0200

    test

--- src/main/js/components/WhenFormSection.js
+++ src/main/js/components/WhenFormSection.js
@@ -37,11 +37,18 @@ class WhenFormSection extends React.Component {
         <FormSectionContainer onClick={() => {this.showModal()}}>
                         When: {this.describeValue()}
         </FormSectionContainer>
-        <MyModal visible={this.state.modalShown} onCancel={() => {this.hideModal()}}>
+        <MyModal dialogClassName="modal-dialog-centered" visible={this.state.modalShown} onCancel={() => {this.hideModal()}}>
             <div className="modal-body" style={{padding: 0}}>
                 {this.currentValue() ?
 
                     <div>
+                        <Calendar
+                            value={this.currentValue().startOf("day")}
+                            onChangeValue={d => {this.onChangeDate(d)}}
+                            style={{
+                                marginBottom: 10
+                            }}
+                        />
                         <div style={{
                             borderTop: "1px solid #666",
                             paddingTop: 10,
