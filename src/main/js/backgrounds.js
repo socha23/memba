d00da8bb977f3e1c530293daabd67d4a7e0b1838
Przemysław Socha <przemyslaw.socha@asseco.pl>
2018-09-06 17:43:27 +0200

    additional backgrounds

--- src/main/js/backgrounds.js
+++ src/main/js/backgrounds.js
@@ -15,6 +15,12 @@ const backgrounds = [
     {name:"Books", value:"books.jpg"},
     {name:"Parcels", value:"parcels.jpg"},
     {name:"Cogs", value:"cogs.jpg"},
+    {name:"Money 1", value:"money1.jpg"},
+    {name:"Money 2", value:"money2.jpg"},
+    {name:"Office 1", value:"office1.jpg"},
+    {name:"Office 2", value:"office2.jpg"},
+    {name:"Tools 1", value:"tools1.jpg"},
+    {name:"Tools 2", value:"tools2.jpg"},
 ];
 
 export const backgroundNoneValue = backgrounds[0].value;
