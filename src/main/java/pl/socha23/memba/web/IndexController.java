0bb84cf3a93b5ca5a45a298fa9cfeb12ed7ef80e
socha23 <socha23@gmail.com>
2018-09-06 23:05:17 +0200

    cachebuster dependent on bundle.js md5, not random

--- src/main/java/pl/socha23/memba/web/IndexController.java
+++ src/main/java/pl/socha23/memba/web/IndexController.java
@@ -1,21 +1,45 @@
 package pl.socha23.memba.web;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import reactor.core.publisher.Mono;
 
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.util.Base64;
 import java.util.Random;
 
 @Controller
 public class IndexController {
 
+    private Resource bundleJs = new ClassPathResource("classpath:/static/bundle.js");
+
     private Random random = new Random();
     private String googleClientId;
 
+    private final String bundleJsMd5;
+
     public IndexController(@Value("${memba.security.google.client_id}") String googleClientId) {
         this.googleClientId = googleClientId;
+        bundleJsMd5 = calculateBundleJsMd5();
+    }
+
+    private String calculateBundleJsMd5() {
+        try {
+            URL resource = getClass().getClassLoader().getResource("static/bundle.js");
+            byte[] bytes = Files.readAllBytes(Paths.get(resource.toURI()));
+            MessageDigest digest = MessageDigest.getInstance("MD5");
+            digest.update(bytes);
+            return Base64.getEncoder().encodeToString(digest.digest());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     @GetMapping("/")
@@ -30,7 +54,7 @@ public class IndexController {
 
     @ModelAttribute("cachebuster")
     public String getCachebuster() {
-        return "" + random.nextInt();
+        return bundleJsMd5;
     }
 
 }
